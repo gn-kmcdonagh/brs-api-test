b5a32d04272910441b3056f476d0e83472a4178d
gn-kmcdonagh <kane.mcdonagh@nbcuni.com>
2023-07-21 15:54:40 +0100

    first commit

---
+++ challenge.php
@@ -0,0 +1,51 @@
+<?php
+$username='brsmobileapp';
+$password='QB6T9vmVw8WcbTZv8bGUJMs2E3SKU4jLBNJscNMZx4hyZdwfcta9ALqS4THXVxw679BFh5FsPEYbSUfHxSmvJVX9Wfm5gCPmterEJTWMzUjt8ej3kLwEbP7CkhLbfVAC8FvaxYDx2KzVvttMDsWxhNzmVqeqvjmVbFSNA2LgkfyDXTSD7XbPpNQRyZQcgzHSRuQVSQ5SZujDYjfm2TnetbP6Dk8gbf7Pae9vWA4kg5Bj84SsshnqMgGTFjPVeYr8';
+$URL='https://www.brsgolf.com/api/v2/clubs';
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$URL);
+curl_setopt($ch, CURLOPT_TIMEOUT, 30); //timeout after 30 seconds
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+curl_setopt($ch, CURLOPT_USERPWD, "$username:$password");
+$result=curl_exec ($ch);
+$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);   //get status code
+curl_close ($ch);
+
+
+// Initiate curl session in a variable (resource)
+$curl_handle = curl_init();
+
+// Set the curl URL option
+curl_setopt($curl_handle, CURLOPT_URL, $URL);
+
+// This option will return data as a string instead of direct output
+curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, true);
+
+// Execute curl & store data in a variable
+$curl_data = curl_exec($curl_handle);
+
+curl_close($curl_handle);
+
+// Decode JSON into PHP array
+$response_data = json_decode($curl_data);
+
+// Print all data if needed
+// print_r($response_data);
+// die();
+
+// All club data exists in 'data' object
+$club_data = $response_data->data;
+
+// Extract only first 50 user data
+$club_data = array_slice($club_data, 0, 49);
+
+// Traverse array and print employee data
+foreach ($club_data as $club) {
+    echo "name: " . $club->club_name;
+    echo "<br />";
+}
+
+
+
