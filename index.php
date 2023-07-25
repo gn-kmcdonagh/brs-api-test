6388c955735d41e2fe44fab867ebe2df65eb6213
gn-kmcdonagh <kane.mcdonagh@nbcuni.com>
2023-07-25 15:20:59 +0100

    update

--- index.php
+++ index.php
@@ -14,10 +14,12 @@ $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);   //get status code
 curl_close ($ch);
 
 $data = json_decode($result);
+$address = json_decode($result);
 
 
 // All club data exists in 'data' object
 $club_data = $data->_results;
+$club_add = $address->_results;
 
 ?>
 <!DOCTYPE html>
@@ -25,7 +27,7 @@ $club_data = $data->_results;
 <head>
     <meta charset="UTF-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Page Title</title>
+    <title>BRS clubs list</title>
 </head>
 <body>
 
@@ -37,7 +39,8 @@ $club_data = $data->_results;
 
     </header>
 </div>
-
+<?php foreach ($club_data as $club):
+echo $club->name;?>
 <div class="py-5 bg-body-tertiary">
     <div class="container">
 
@@ -46,84 +49,86 @@ $club_data = $data->_results;
             <div class="col">
                 <div class="card shadow-sm h-100">
                     <div class="card-header">
-                        Aberdovey Golf Club
+                        <?php echo $club_data->name;?>
                     </div>
                     <div class="card-body">
                         <p class="card-text">
-                            <?php foreach ($club_data as $data){
-                                echo $club_data->name[''];
-                            }?>
-                        </p>
-                        <div class="d-flex justify-content-between align-items-center">
-                            <div class="btn-group">
-                                <button type="button" class="btn btn-sm btn-outline-secondary">Visit</button>
-                            </div>
-                            <small class="text-body-secondary">(aberdovey)</small>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
-            <div class="col">
-                <div class="card shadow-sm h-100">
-                    <div class="card-header">
-                        Abergele Golf Club
-                    </div>
-                    <div class="card-body">
-                        <p class="card-text">
-                            Tan-y-Gopa Road, Abergele, Conwy, Wales, LL22 8DS
-                        </p>
-                        <div class="d-flex justify-content-between align-items-center">
-                            <div class="btn-group">
-                                <button type="button" class="btn btn-sm btn-outline-secondary">Visit</button>
-                            </div>
-                            <small class="text-body-secondary">(abergele)</small>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
-            <div class="col">
-                <div class="card shadow-sm h-100">
-                    <div class="card-header">
-                        Abridge Golf Club
-                    </div>
-                    <div class="card-body">
-                        <p class="card-text">
-                            Epping Lane, Stapleford Tawney, Essex, England, RM4 1ST
-                        </p>
-                        <div class="d-flex justify-content-between align-items-center">
-                            <div class="btn-group">
-                                <button type="button" class="btn btn-sm btn-outline-secondary">Visit</button>
-                            </div>
-                            <small class="text-body-secondary">(abridge)</small>
-                        </div>
-                    </div>
-                </div>
-            </div>
+</p>
+<div class="d-flex justify-content-between align-items-center">
+    <div class="btn-group">
+        <button type="button" class="btn btn-sm btn-outline-secondary">Visit</button>
+    </div>
+    <small class="text-body-secondary">(aberdovey)</small>
+</div>
+</div>
+</div>
+</div>
 
-            <div class="col">
-                <div class="card shadow-sm">
-                    <div class="card-header">
-                        Adare Manor Golf Club
-                    </div>
-                    <div class="card-body">
-                        <p class="card-text">
-                            Adare (Old Course), Limerick, Ireland
-                        </p>
-                        <div class="d-flex justify-content-between align-items-center">
-                            <div class="btn-group">
-                                <button type="button" class="btn btn-sm btn-outline-secondary">Visit</button>
-                            </div>
-                            <small class="text-body-secondary">(adaremanor)</small>
-                        </div>
-                    </div>
+<div class="col">
+    <div class="card shadow-sm h-100">
+        <div class="card-header">
+            Abergele Golf Club
+        </div>
+        <div class="card-body">
+            <p class="card-text">
+                Tan-y-Gopa Road, Abergele, Conwy, Wales, LL22 8DS
+            </p>
+            <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Visit</button>
                 </div>
+                <small class="text-body-secondary">(abergele)</small>
             </div>
-
         </div>
     </div>
 </div>
+
+<div class="col">
+    <div class="card shadow-sm h-100">
+        <div class="card-header">
+            Abridge Golf Club
+        </div>
+        <div class="card-body">
+            <p class="card-text">
+                Epping Lane, Stapleford Tawney, Essex, England, RM4 1ST
+            </p>
+            <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Visit</button>
+                </div>
+                <small class="text-body-secondary">(abridge)</small>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="col">
+    <div class="card shadow-sm">
+        <div class="card-header">
+            Adare Manor Golf Club
+        </div>
+        <div class="card-body">
+            <p class="card-text">
+                Adare (Old Course), Limerick, Ireland
+            </p>
+            <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Visit</button>
+                </div>
+                <small class="text-body-secondary">(adaremanor)</small>
+            </div>
+        </div>
+    </div>
+</div>
+
+</div>
+</div>
+</div>
+
+
+<?php endforeach;?>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
