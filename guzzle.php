1bebf0696613b4fced5e00144f16cb7107e0c7bb
gn-kmcdonagh <kane.mcdonagh@nbcuni.com>
2023-07-26 15:01:42 +0100

    implemented success with returning no clubs + not Successful states message.

--- guzzle.php
+++ guzzle.php
@@ -1,5 +1,8 @@
 <?php
 
+
+global $data;
+
 use GuzzleHttp\Client;
 
 $username = 'brsmobileapp';
@@ -18,45 +21,94 @@ $response = $client->request('GET', $URL, [
     ]
 ]);
 
+//
+//if (200 == $response->getStatusCode()) {
+//    $name = $response->getBody();
+//
+//    $data = json_decode($name);
+//    $clubData =$data->_results;
+//
+//         foreach ($clubData as $club):
+//        $uri = 'https://www.brsgolf.com/' . $club->club_id;
+//        ?>
+<!--        <li>-->
+<!---->
+<!--            --><?php //= $club->name; ?>
+<!--            <a href="--><?php //echo $uri ?><!--">-->
+<!--                --><?php //echo "(" . $uri . $club->club_id . ")"; ?>
+<!--            </a>-->
+<!--        </li>-->
+<!---->
+<!---->
+<!---->
+<!---->
+<!---->
+<!-- --><?php //endforeach;
+//} else {
+//echo ' no clubs found';
+//}
 
-if (200 == $response->getStatusCode()) {
-    $name = $response->getBody();
-
-    $data = json_decode($name);
-
-} else {
-    echo 'Fix if statement';
-}
-
-
-$club_data = $data->_results;
 
 
 ?>
 <html>
 <body>
 <style>
-    h1{font-family: sans-serif; font-size: 50px; padding: 40px; margin-bottom: 10px; margin-left: 0;  width: 100%}
-    li{list-style: none; font-size: 20px; font-family: sans-serif;}
-    body{background: #30E8BF;  /* fallback for old browsers */
-        background: -webkit-linear-gradient(to right, #FF8235, #30E8BF);  /* Chrome 10-25, Safari 5.1-6 */
+    h1 {
+        font-family: sans-serif;
+        font-size: 50px;
+        padding: 40px;
+        margin-bottom: 10px;
+        margin-left: 0;
+        width: 100%;
+        text-align: center;
+        display: flex;
+    }
+
+    li {
+        list-style: none;
+        font-size: 20px;
+        font-family: sans-serif;
+
+    }
+
+    body {
+        background: #30E8BF; /* fallback for old browsers */
+        background: -webkit-linear-gradient(to right, #FF8235, #30E8BF); /* Chrome 10-25, Safari 5.1-6 */
         background: linear-gradient(to right, #FF8235, #30E8BF); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
     }
 </style>
 <ul>
     <h1>BRS GOLF CLUB LINKS</h1>
 
-<?php foreach ($club_data as $club):
-    $uri = 'https://www.brsgolf.com/' . $club->club_id;
-    ?>
-    <li>
+<?php
+if (200 == $response->getStatusCode()) {
+    $name = $response->getBody();
+
+    $data = json_decode($name);
+    $clubData =[];
+
+         foreach ($clubData as $club):
+        $uri = 'https://www.brsgolf.com/' . $club->club_id;
+        ?>
+        <li>
 
             <?= $club->name; ?>
-        <a href="<?php echo $uri?>">
-        <?php echo "(".$uri.$club->club_id.")"; ?>
-        </a>
-    </li>
-<?php endforeach; ?>
-</ul>
+            <a href="<?php echo $uri ?>">
+                <?php echo "(" . $uri . $club->club_id . ")"; ?>
+            </a>
+        </li>
+
+
+
+
+
+ <?php endforeach;
+} else {?>
+<h2><?php echo ' No clubs found';
+}?>
+</h2>
+
+
 </body>
 </html>
